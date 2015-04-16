b0660faab258917d380e6fbe0938a6355b0b4c27
gocsp <rudidiedrich@me.com>
2015-04-16 09:20:46 +0200

    hacky... csrf exempt

--- jserrorlogging/urls.py
+++ jserrorlogging/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls import url
 from django.conf.urls import patterns
 from .views import LoggingView
 
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = patterns(
     '',
-    url(r'^add/$', LoggingView.as_view(), name='add_log'),
+    url(r'^add/$', csrf_exempt(LoggingView.as_view()), name='add_log'),
 )
